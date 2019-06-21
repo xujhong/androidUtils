464e4af39b0914abb45b8db76203fb3da868619f
616876163@qq.com <hong320.+>
2019-06-21 16:54:40 +0800

    测试提交代码

--- app/src/main/java/com/xjh/androidutils/MainActivity.java
+++ app/src/main/java/com/xjh/androidutils/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //测试提交代码
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
